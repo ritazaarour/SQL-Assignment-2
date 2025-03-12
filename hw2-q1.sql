41e0d5c37ff9c5ef3863b0f9e25ad9dd4ee6e20c
Rita Zaarour <ritazaarour9@gmail.com>
2025-03-11 20:21:34 -0700

    hw2-q1.sql

--- hw2-q1.sql
+++ hw2-q1.sql
@@ -1,3 +1,4 @@
+/* For each origin city, find the destination city (or cities) with the longest direct flight. By direct flight, we mean a flight with no intermediate stops. Judge the longest flight in time, not distance.*/
 SELECT DISTINCT F.origin_city, F.dest_city, F.actual_time AS time 
 FROM Flights F 
 JOIN (SELECT origin_city, MAX(actual_time) AS time 
