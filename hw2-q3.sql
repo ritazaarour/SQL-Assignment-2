96ee8ca43f7996ff49325bedefa41584b82e4771
Rita Zaarour <ritazaarour9@gmail.com>
2025-03-11 20:23:25 -0700

    hw2-q3.sql

--- hw2-q3.sql
+++ hw2-q3.sql
@@ -1,3 +1,5 @@
+/* For each origin city, find the percentage of departing flights shorter than 3 hours. For this 
+question, treat flights with NULL actual_time values as longer than 3 hours. */
 SELECT origin_city, SUM(CASE WHEN actual_time < 180 THEN 1 ELSE 0 END)*100 / COUNT(*) AS percentage 
 FROM Flights 
 GROUP BY origin_city 
