2cf775956c36c286c0020dce88a9c76de2a93d17
Rita Zaarour <ritazaarour9@gmail.com>
2025-03-11 20:26:50 -0700

    hw2-q7.sql

--- hw2-q7.sql
+++ hw2-q7.sql
@@ -1,3 +1,5 @@
+/* List all tracks that were never purchased by any customers. 
+Return distinct track names. We only need the name attribute. */
 SELECT DISTINCT T.name AS songs 
 FROM track T WHERE T.track_id NOT IN 
     (SELECT T2.track_id FROM track T2 JOIN invoice_line il ON T.track_id = il.track_id WHERE T2.track_id = IL.track_id )
