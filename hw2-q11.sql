9d39ae7c50cc4e207bf7ea9f8b6bcbb7bf9f26d5
Rita Zaarour <ritazaarour9@gmail.com>
2025-03-11 20:29:01 -0700

    hw2-q11.sql

--- hw2-q11.sql
+++ hw2-q11.sql
@@ -1,3 +1,5 @@
+/* Find the list of artists that record at least in 3 different 
+genres. Again, return only the artist name. */
 SELECT A.name 
 FROM artist A 
 JOIN album AL ON A.artist_id = AL.artist_id 
