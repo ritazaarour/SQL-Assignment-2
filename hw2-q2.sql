763a8903764bbb5abcff851ec46eafcefb266c06
Rita Zaarour <ritazaarour9@gmail.com>
2025-03-11 20:22:46 -0700

    hw2-q2.sql

--- hw2-q2.sql
+++ hw2-q2.sql
@@ -1,3 +1,6 @@
+/* Find all origin cities that only serve flights shorter than 3 hours. 
+You can assume that flights with NULL actual_time are not 3 hours or more.
+Name the output column city and sort them. List each city only once in the result. */
 SELECT origin_city AS city 
 FROM Flights 
 GROUP BY origin_city 
