eedbaf83a4c5dc4623f77879da328b0fabdca456
Rita Zaarour <ritazaarour9@gmail.com>
2025-03-11 20:27:26 -0700

    hw2-q8.sql

--- hw2-q8.sql
+++ hw2-q8.sql
@@ -1,3 +1,5 @@
+/* List the names of all songs that do not belong to the 90s Music 
+playlist. Return distinct track names (only the name attribute). */
 SELECT DISTINCT T.name 
 FROM track T 
 JOIN playlist_track PT ON T.track_id = PT.track_id 
